fedc2f350f85a6a0d91ee1651b2f1d13a8693b39
OpenTech-Consult <opentech.consult@gmail.com>
2022-09-20 07:41:51 +0000

    Multiplexing an demultiplexing
    created a program that start a child process and redirect both its standard output and error to a remote server which in turn saves the two stream into separate files

---
+++ 06-coding-with-stream/25-multiplexing/README.md
@@ -0,0 +1,13 @@
+# 25-multiplexing
+
+This example demonstrates how to perform multiplexing and demultiplexing using Node.js streams.
+
+## Run
+
+To run the example you need to start a server in a terminal window with:
+
+> node server.js
+
+Then you can start a client:
+
+> node client.js generate-data.js
---
+++ 06-coding-with-stream/25-multiplexing/server.js
@@ -0,0 +1,46 @@
+import { createWriteStream } from 'node:fs'
+import { createServer } from 'node:net'
+
+function demultiplexChannel(source, destinations) {
+    let currentChannel = null
+    let currentLength = null
+
+    source
+        .on('readable', () => {
+            let chunk
+            if (currentChannel === null) {
+                chunk = source.read(1)
+                currentChannel = chunk && chunk.readUInt8(0)
+            }
+
+            if (currentLength === null) {
+                chunk = source.read(4)
+                currentLength = chunk && chunk.readUInt32BE(0)
+                if (currentLength === null) {
+                    return null
+                }
+            }
+
+            chunk = source.read(currentLength)
+            if (chunk === null) {
+                return null
+            }
+
+            console.log(`Received packet from: ${currentChannel}`)
+            destinations[currentChannel].write(chunk)
+            currentChannel = null
+            currentLength = null
+        })
+        .on('end', () => {
+            destinations.forEach(destination => destination.end())
+            console.log('Source channel closed')
+        })
+}
+
+const server = createServer((socket) => {
+    console.log('Socket received')
+    const stdoutStream = createWriteStream('stdout.log')
+    const stderrStream = createWriteStream('stderr.log')
+    demultiplexChannel(socket, [stdoutStream, stderrStream])
+})
+server.listen(3000, () => console.log('Server started.'))
